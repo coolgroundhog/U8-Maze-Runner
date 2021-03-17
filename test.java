7aa6c12d37484fce923dab6554e06f99d5e150a4
Brian Chiang <briyanchiang@gmail.com>
2021-03-17 23:09:05 +0800

    josh: removed test.java

--- test.java
+++ test.java
@@ -1,5 +1,30 @@
 public class test{
     public static void main(String[] args){
         System.out.println("test 2 for unit 8");
+
+        int myArray[][] = {{0, 0, 3}, {0, 0, 0}, {7, 0, 0}};
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                System.out.print(myArray[i][j]);
+            }
+            System.out.println();
+        }
+      
+        
+
+        int[][] arr = {{3, 2, 1}, {4, 3, 5}};
+ 
+        for (int row = 0; row < arr.length; row++){
+            for (int col = 0; col < arr[row].length; col++){
+                if (col > 0){
+                    if (arr[row][col] >= arr[row][col - 1]){
+                        System.out.println("Condition one");
+                    }
+                }
+                if (arr[row][col] % 2 == 0){
+                    System.out.println("Condition two");
+                }
+            }
+        }
     }
 }
